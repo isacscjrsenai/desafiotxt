4048dd5cdd42b46c3da5eae0531c48a0e4b242ca
isacscjrsenai <isac.s.junior@aluno.senai.br>
2023-04-26 18:02:23 -0300

    desafio arquivo.txt

---
+++ ConsoleApp1/OS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class OS
+    {
+        int id,solicitacao,prazo;
+        string produto,servico;
+        DateTime data;
+        
+    }
+}
